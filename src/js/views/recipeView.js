2e8f8b19eca59f236cee4d540883acf093c2ac46
dmiky-xamk <dmiky001@edu.xamk.fi>
2021-11-06 18:24:46 +0200

    New fraction import

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -130,13 +130,12 @@ class RecipeView extends View
 
     _generateMarkupIngredient(ing)
     {
-        console.log(new Fraction(1.88));
         return `
         <li class="recipe__ingredient">
             <svg class="recipe__icon">
             <use href="${icons}#icon-check"></use>
             </svg>
-            <div class="recipe__quantity">${ing.quantity ? ing.quantity.toString() : ""}</div>
+            <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity.toString()).toFraction(true) : ""}</div>
             <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
             ${ing.description}
